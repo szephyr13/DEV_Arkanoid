4f3b6329685e09f7ad795cd287267e3eb7bea02f
Azazel <80421328+szephyr13@users.noreply.github.com>
2024-11-23 23:17:51 +0100

    ball movement

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ball movement start when space pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //1. quits parent
+            transform.SetParent(null);
+            //2. sets ball as dynamic (physics)
+            rb.isKinematic = false;
+            //3. applies impulse
+            rb.AddForce(new Vector2(1, 1).normalized * 10, ForceMode2D.Impulse);
+
+        }
+    }
+}
